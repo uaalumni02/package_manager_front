5405e6584c349632b5bd393d4ee7252d013bbd10
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-10-17 21:10:01 -0400

    add login functionality

--- src/logIn/logIn.js
+++ src/logIn/logIn.js
@@ -9,8 +9,35 @@ class Login extends Component {
             username: '',
             password: '',
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        const { username, password } = this.state;
+        fetch('http://localhost:3000/api/user/login', {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+            .then(res => res.json())
+            .then(response => {
+                console.log(response)
+            }).catch(error => console.error('Error:', error));
+        this.setState({ username: '', password: '' })
+    }
 
     render() {
         return (
@@ -19,13 +46,13 @@ class Login extends Component {
                     <div className="col-12 col-md-5 auth-box">
                         <h3 className="text-center mt-4">Log in to your account</h3>
                         <p className="text-center text-danger mt-4"> </p>
-                        <form className="px-3 py-5">
+                        <form id="submitForm" className="px-3 py-5" onSubmit={this.handleSubmit}>
                             <div className="form-group">
                                 <div className="input-group">
                                     <div className="input-group-prepend">
                                         <div className="input-group-text"></div>
                                     </div>
-                                    <input type="text" name="username" className="form-control form-control-lg" id="username" placeholder="Username" />
+                                    <input type="text" name="username" value={this.state.username} onChange={this.handleChange} className="form-control form-control-lg" id="username" placeholder="Username" />
                                 </div>
                             </div>
                             <div className="form-group">
@@ -33,7 +60,7 @@ class Login extends Component {
                                     <div className="input-group-prepend">
                                         <div className="input-group-text"><i className="ion ion-eye"></i></div>
                                     </div>
-                                    <input type="password" name="password" className="form-control form-control-lg" id="password" placeholder="Password" />
+                                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} className="form-control form-control-lg" id="password" placeholder="Password" />
                                 </div>
                             </div>
                             <br></br>
