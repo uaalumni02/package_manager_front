8270b455a1fc1734b8cfb5a41749674cddea34c0
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-11-10 19:15:58 -0500

    add useEffect

--- src/logIn/logIn.js
+++ src/logIn/logIn.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './login.css';
 
 const LoginForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    useEffect(() => {
+    }, [username, password])
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -21,8 +23,6 @@ const LoginForm = () => {
             .then(res => res.json())
             .then(response => {
                 console.log(response)
-                setUsername();
-                setPassword()
             }).catch(error => console.error('Error:', error));
     }
     return (
