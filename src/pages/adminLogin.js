86ff5e2818e9fc1017d82c2b3d5595ea569bb0f0
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2020-02-18 20:58:29 -0500

    fix route error

--- src/pages/adminLogin.js
+++ src/pages/adminLogin.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
-import Button from  "../components/Button"
+import Button from  "../components/button"
 
 import {
   MDBContainer,
