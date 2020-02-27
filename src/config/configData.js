28fa6cce71e0becd6bd6bd071a31b65c6aba13b7
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2020-02-26 22:32:54 -0500

    add config

---
+++ src/config/configData.js
@@ -0,0 +1,14 @@
+// const port = 3000
+// const configData = {
+//   local: {
+//     env: 'local',
+//     tzApiUrl: `http://localhost:${port}`,
+//     prodUrl: "https://tz-package-manager.herokuapp.com",
+//   },
+// }
+
+// export default configData
+
+let prodUrl = "https://tz-package-manager.herokuapp.com";
+
+export {prodUrl};
